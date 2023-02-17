f358f418e8f45d2ee677c8e1cdf55a3e2c7fdac7
Pranav Arora <aurorapranav187@gmail.com>
2023-02-17 14:47:11 -0330

    Update exp date

--- src/api.js
+++ src/api.js
@@ -27,7 +27,7 @@ router.post("/", (req, res) => {
 
     language: "eng",
 
-    exp: 1616239022, // This will automatically expire the jwt once it is created and used for ASA24. Comment this to run the simulation.
+    exp: 1676657197, // This will automatically expire the jwt once it is created and used for ASA24. Comment this to run the simulation.
 
     iss: "sugarmun",
 
